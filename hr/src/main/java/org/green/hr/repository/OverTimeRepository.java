5fd8feb2834b80f3d7c1e0e56067c9da0135e160
VietAnh <89052171+luongthanhhuy@users.noreply.github.com>
2024-09-06 22:38:19 +0700

    Update repository

--- hr/src/main/java/org/green/hr/repository/OverTimeRepository.java
+++ hr/src/main/java/org/green/hr/repository/OverTimeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OverTimeRepository extends JpaRepository<Overtime, Integer> {
+public interface OverTimeRepository extends JpaRepository<Overtime, Long> {
 }
