4273874ba986a4c51b57a79dc553e8c2478892f6
phong <ngthephong2001>
2024-09-09 10:25:10 +0700

    Update liquibase and entity of qualification

--- hr/src/main/java/org/green/hr/dto/QualificationDTO.java
+++ hr/src/main/java/org/green/hr/dto/QualificationDTO.java
@@ -1,4 +1,13 @@
 package org.green.hr.dto;
 
+import java.util.Date;
+
 public class QualificationDTO {
+    private Long id;
+    private String qualificationName;
+    private Date expiryDate;
+    private String image;
+    private Short status;
+    private Date createAt;
+    private Date updateAt;
 }
