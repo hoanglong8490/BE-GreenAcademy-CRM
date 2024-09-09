c8576283bfe226da38ec64a69c6ae99e45c05c1d
phong <ngthephong2001>
2024-09-09 09:22:28 +0700

    update contract

--- hr/src/main/java/org/green/hr/dto/ContractDTO.java
+++ hr/src/main/java/org/green/hr/dto/ContractDTO.java
@@ -2,19 +2,17 @@ package org.green.hr.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Date;
-import java.util.List;
 
-@NoArgsConstructor
 @Data
+@Builder
 public class ContractDTO {
     private Long id;
     private String contractCode;
     private String employeeCode;
     private String contractCategory;
-    private List<String> contentContract;
+    private String contentContract;
     private Float salary;
     private Date dateStart;
     private Date dateEnd;
