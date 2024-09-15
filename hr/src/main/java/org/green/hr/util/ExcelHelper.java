53b96cbf6448cb716633e90a837da0443cf964e9
VietAnh <89052171+luongthanhhuy@users.noreply.github.com>
2024-09-15 23:34:09 +0700

    create api for position

--- hr/src/main/java/org/green/hr/util/ExcelHelper.java
+++ hr/src/main/java/org/green/hr/util/ExcelHelper.java
@@ -2,6 +2,7 @@ package org.green.hr.util;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.green.hr.dto.PositionDTO;
 import org.green.hr.dto.QualificationDTO;
 
 import java.io.InputStream;
@@ -13,7 +14,7 @@ import java.util.List;
 
 public class ExcelHelper {
 
-    public static List<QualificationDTO> excelToEntities(InputStream is) {
+    public static List<QualificationDTO> excelToQualificationEntities(InputStream is) {
         try {
             Workbook workbook = new XSSFWorkbook(is);
             Sheet sheet = workbook.getSheetAt(0);
@@ -41,6 +42,34 @@ public class ExcelHelper {
 
             workbook.close();
             return qualificationDTOs;
+        } catch (Exception e) {
+            throw new RuntimeException("fail to parse Excel file: " + e.getMessage());
+        }
+    }
+    
+    public static List<PositionDTO> excelToPositionEntities(InputStream is) {
+        try {
+            Workbook workbook = new XSSFWorkbook(is);
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rows = sheet.iterator();
+
+            List<PositionDTO> positionDTOs = new ArrayList<>();
+
+            rows.next();
+
+            while (rows.hasNext()) {
+                Row row = rows.next();
+                PositionDTO dto = new PositionDTO();
+
+                dto.setPositionName(row.getCell(0).getStringCellValue());
+                dto.setDepartmentName(row.getCell(1).getStringCellValue());
+                dto.setStatus((short) 1);
+
+                positionDTOs.add(dto);
+            }
+
+            workbook.close();
+            return positionDTOs;
         } catch (Exception e) {
             throw new RuntimeException("fail to parse Excel file: " + e.getMessage());
         }
