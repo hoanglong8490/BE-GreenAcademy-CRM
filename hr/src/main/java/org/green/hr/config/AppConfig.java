5e31119ac6e6ccdefc84c4cfe9def752a8628109
hoanglong8490 <hoanglong8490@gmail.com>
2024-09-16 15:05:37 +0700

    Your commit message

--- hr/src/main/java/org/green/hr/config/AppConfig.java
+++ hr/src/main/java/org/green/hr/config/AppConfig.java
@@ -4,15 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.modelmapper.ModelMapper;
 
 @Configuration
 @ComponentScan(basePackages = {"org.green.core"})
 public class AppConfig {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
